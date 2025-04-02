99a2815a574899bbe8104546b02bb03990a57f5a
despkontopoulou <despkontopoulou@gmail.com>
2025-04-02 19:53:49 +0300

    ui fixes

--- routes/home.py
+++ routes/home.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 home_bp=Blueprint('home_bp',__name__)#create route for home
 @home_bp.route('/')
 def index():
-    return render_template("home/index.html")
+    return redirect(url_for('pathfinding.select_points'))  # Redirect to point selection
